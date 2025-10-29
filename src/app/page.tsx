f6861ef7936174d4af395be830b8e981e1dc78aa
Juke <68906708+Jukeeeee@users.noreply.github.com>
2025-10-30 00:49:57 +0800

    Minor Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -313,7 +313,7 @@ export default function Home() {
 
       {/* -----------------------------------------------------------------
           ABOUT SECTION */}
-      <section className="py-20 px-4 sm:px-6 lg:px-8 bg-black/5">
+      <section className="py-20 px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           <motion.div
             className="text-center mb-12"
